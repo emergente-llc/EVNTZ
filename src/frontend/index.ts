77bb88e9030aedb6fa57b558cd24488720d64354
Richard N. Roman <richardn.roman@gmail.com>
2024-06-24 00:29:24 -0400

    NEW Include Bootstrap framework for style.
    UPDATE
    New elements included in the frontend index.html and index.ts files.
    Now the user can see the qty of transactions generated or received by the API POST method.

--- src/frontend/index.ts
+++ src/frontend/index.ts
@@ -2,9 +2,7 @@ import { html, LitElement } from "lit";
 import { customElement, property } from "lit/decorators.js";
 
 @customElement("azle-app")
-
 export class AzleApp extends LitElement {
-
   @property()
   sortedTransactions = {};
 
@@ -26,20 +24,48 @@ export class AzleApp extends LitElement {
 
   render() {
     return html`
-      <h1>EVNTZ - Sales Transaction dashboard</h1>
+      <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+        crossorigin="anonymous"
+      />
+      <div class="container">
+        <div class="row g-3">
+          <div class="col-md-8">
+            <div class="alert alert-primary" role="alert">
+              <h3>EVNTZ - Sales Transaction dashboard</h3>
+            </div>
 
-      <div style="width: 50%;">
-        Transactions: 
-        <br>
-        <pre style="white-space: pre-wrap;">
-          ${JSON.stringify(this.sortedTransactions, null, 2)}
-        </pre>
-      </div>
-      <br />
-      <div>
-        <button>
-          @click=${this.getAllTransaction}>Test /transactions
-        </button>        
+            <button type="button" class="btn btn-light position-relative">
+              <h5>Transactions</h5>
+              <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-primary">
+                ${Object.keys(this.sortedTransactions).length}
+                <span class="visually-hidden">All transactions</span>
+              </span>
+            </button>
+
+            <br />
+            <br />
+            
+            <pre style="white-space: pre-wrap;">
+              ${JSON.stringify(this.sortedTransactions, null, 2)}
+            </pre>
+
+            <br />
+
+            <button
+              type="button"
+              class="btn btn-primary"
+              @click=${this.getAllTransaction}
+            >
+              Fetch all transactions
+            </button>
+
+            <br />
+            <br />
+          </div>
+        </div>
       </div>
     `;
   }
