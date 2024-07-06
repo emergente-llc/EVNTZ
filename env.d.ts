e8c2a390c11e77a1cc90d98fa40c2956f8f4f4f9
Richard N. Roman <richardn.roman@gmail.com>
2024-07-05 21:16:47 -0400

    UPDATE ENV NFT canister in env.d.ts

--- env.d.ts
+++ env.d.ts
@@ -5,6 +5,7 @@ interface ImportMetaEnv {
     readonly ACCESS_TOKEN_SECRET: string;
     readonly RS_SEC_HDR_VENDOR_ID: string;
     readonly RS_SEC_HDR_VENDOR_PASSWORD: string;
+    readonly NFT_ID: string;
 }
 
 interface ImportMeta {
